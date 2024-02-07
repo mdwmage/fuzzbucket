f7cdf043b74f847ddadeddfad94fd38d57f54f65
mdw <mdwmage@proton.me>
2024-02-07 05:42:13 -0500

    feat(args): arg parsing
    basic parsing of cli args, currently supports showing version and help command. help command provides a sort of roadmap of sorts

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
-
 using namespace std;
 
+const char VERSION[] = "0.0.1";
+
+void checkArgs(int, string[]);
+
 int main(int argc, char *argv[]) {
-  cout << "fuzzbucket package manager" << endl;
+  // Convert args to strings
+  string args[argc];
+  for (int i = 0; i < argc; i++) {
+    args[i] = argv[i];
+  }
+  // Compute Args
+  if (argc >= 2) {
+    cout << "Fuzzbucket Package Manager" << endl;
+    checkArgs(argc, args);
+  }
+}
+
+void checkArgs(int argc, string args[]) {
+  if (args[1].compare("help") == 0) {
+    cout << "Commands:" << endl;
+    cout << "- build: generates build files for project" << endl
+         << "- get {url}: adds repo to dependency list and downloads it" << endl
+         << "- help: displays this output" << endl
+         << "- init: create new Fuzzbucket project" << endl
+         << "- list: lists packages installed" << endl
+         << "- version: shows fuzzbucket version" << endl;
+  } else if (args[1].compare("version") == 0) {
+    cout << "Version: " << VERSION << endl;
+  }
 }
