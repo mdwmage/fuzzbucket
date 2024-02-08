cd90ca4d826591c3bec19c553d18c937110f7b2e
phoenix <phoenixwb@netc.eu>
2024-02-08 09:54:08 -0500

    feat(Logger): add Transport classes and Logger class
    - Created a Transport base class with a virtual send method
    - Created Console and File classes that inherit from Transport and override the send method
    - Created a Logger class that uses a vector of Transport pointers to send messages

---
+++ Logger.h
@@ -0,0 +1,70 @@
+//
+// Created by phoenix on 07/02/24.
+//
+
+#ifndef FUZZBUCKET_LOGGER_H
+#define FUZZBUCKET_LOGGER_H
+
+#include <vector>
+#include "iostream"
+#include "fstream"
+
+using namespace std;
+
+namespace UI {
+
+    class Transport {
+    public:
+        virtual void send(const char* message) {
+            throw "Cannot log to empty Transport";
+        };
+    };
+
+    class Console : public Transport {
+    public:
+        void send(const char* message) override {
+            printf("%s\n", message);
+        };
+    };
+
+    class File : public Transport {
+        ofstream target;
+    public:
+        File(const char* target) {
+            this-> target.open(target);
+        }
+        void send(const char* message) override {
+            target << message << endl;
+        };
+        void close() {
+            target.close();
+        }
+    };
+
+    class Logger {
+        vector<UI::Transport*> Targets;
+        void send(const string& message, const string& type) {
+            for (auto & target : Targets) {
+                target->send((type + "    " +  message).c_str());
+            }
+        }
+    public:
+        void addTransport(UI::Transport* target) {
+            Targets.push_back(target);
+        }
+        explicit Logger(Transport* targets[]) {
+            for (int i = 0; i < sizeof(targets); i++) {
+                addTransport(targets[i]);
+            }
+        }
+        void info(const char* message) {
+            send(message, "INFO");
+        }
+        void warn(const char* message) {
+            send(message, "WARN");
+        }
+    };
+
+} // log
+
+#endif //FUZZBUCKET_LOGGER_H
---
+++ logger.cpp
@@ -0,0 +1,8 @@
+//
+// Created by phoenix on 07/02/24.
+//
+
+#include "Logger.h"
+
+namespace log {
+} // log
